3c1689345153136105adeec94892045e6334c899
michal.krupa <xopeneyes@gmail.com>
2024-08-11 12:28:29 +0200

    names updates

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Dec 28, 2017 15:37:25").getTime();
+	var countDownDate = new Date("Sep 6, 2025 12:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
